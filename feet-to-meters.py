47f456666f402083fdbde1a90cc8675ca6ad3f0a
Nathan Train <nathan_train@hotmail.com>
2022-01-22 16:22:25 -0300

    Playing with tkinter

---
+++ feet-to-meters.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter import ttk
+
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+
+root = Tk()
+# naming the window
+root.title("Feet to Meters")
+
+# frame is what "hold" the widgets
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+# sticky configure the way column/row will expand if a blank space is avaliable
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+# row/column configure tell that the frame should expand with extra space
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# here we set the var that will receive the entry and where the widget will be
+feet = StringVar()
+feetEntry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feetEntry.grid(column=2, row=1, sticky=(W, E))
+
+# setting meters var
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=W)
+
+# setting buttons and texts
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(
+    column=3, row=3, sticky=W)
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+# adding padding to each child of mainframe
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+# for start focus on the entry and to click "Enter" and calculate
+feetEntry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
